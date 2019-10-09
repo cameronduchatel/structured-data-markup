e4bb5f0f1e93a7d8399e4725319cb99b67d0edd7
cameronduchatel <44227798+cameronduchatel@users.noreply.github.com>
2019-10-09 16:55:33 +1000

    Add files via upload
    gtm js wrap for JSON-LD markup

---
+++ JSON-LD Markup/gtm js wrap for JSON-LD markup .js
@@ -0,0 +1,16 @@
+<script>
+(function(){
+	var jsondata = {
+
+
+
+
+};
+
+var el = document.createElement ('script');
+el.type = 'application/ld+json';
+el.innerHTML = JSON.stringify(jsondata);
+document.head.appendChild(el);
+}) ();
+
+</script> 
