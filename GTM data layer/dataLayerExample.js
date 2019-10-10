1349212c952ac686e036556ca1cfc0c8af7986ef
cameronduchatel <44227798+cameronduchatel@users.noreply.github.com>
2019-10-10 13:27:40 +1000

    Add files via upload
    GTM data layer example.
    
    Purpose: using the data layer populated variables as dynamic variables in a html tag for JSON-LD structured markup to enable rich snippets.

---
+++ GTM data layer/dataLayerExample.js
@@ -0,0 +1,34 @@
+
+# GTM USER DEFINED VARIABLES FOR DATA LAYER
+
+# 'Data Layer Variable' created in GTM:
+# [1] dl.product.id
+# [2] dl.product.price
+# [3] dl.product.sku
+# [4] dl.product.title
+# [5] dl.product.type
+
+
+# variable - 'custom javascript', variable configuration - 'product detail view':
+
+function() {
+	var ecommerce = {
+    	'ecommerce': {
+        	'detail': {
+            	'products': [{
+                	'title': '{{dl.product.title}}',
+                  	'id': '{{dl.product.id}}',
+                  	'sku': '{{dl.product.sku}}',
+                  	'price': '{{dl.product.price}}',
+                  	'type': '{{dl.product.type}}'
+                
+                }]
+            }
+        }
+    };
+	return ecommerceData;
+}
+
+
+
+
